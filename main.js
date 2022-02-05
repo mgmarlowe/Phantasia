3310e83d18ae3511ff9489eb79eaec8ec28889ed
Melyssa <m.g.mayhem@gmail.com>
2022-02-05 09:02:24 -0500

    Initial content commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function background () {
+    let homeJumboHeight = document.documentElement.clientHeight;
+    let homeJumboWidth = document.documentElement.clientWidth;
+    // homeJumboHeight = $('#homeJumbo').css('height');
+    homeJumboWidth = $('#homeJumbo').css('width');
+    console.log(homeJumboHeight);
+};
